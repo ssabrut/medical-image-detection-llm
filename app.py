79f9c459d53cf3737dfdcaf2b5af4891de92b356
ssabrut <michael.gunawan2002@gmail.com>
2024-07-08 20:35:39 +0700

    Add config for model

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import os
+
 import streamlit as st
 import google.generativeai as genai
 
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+
+# configure genai
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+generation_config = {
+    'temperature': .4,
+    'top_p': 1,
+    'top_k': 32,
+    'max_output_tokens': 4096
+}
 
 # set page config
 st.set_page_config(page_title="Medical Image Analysis", page_icon=":robot:")
