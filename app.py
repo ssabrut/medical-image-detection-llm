160e1806b98ec0b997422bfe4c8275f51e16b084
ssabrut <michael.gunawan2002@gmail.com>
2024-07-08 20:32:29 +0700

    Add submit button

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # set page config
-st.set_page_config(page_title="Vital Image Analysis", page_icon=":robot:")
+st.set_page_config(page_title="Medical Image Analysis", page_icon=":robot:")
 
 # set title
-st.title("👨‍⚕️ Vital Image Analysis")
+st.title("👨‍⚕️ Medical Image Analysis")
 
 # set subtitle
 st.subheader("An AI-powered tool to analyze medical images")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+submit_button = st.button("Analyze")
+if submit_button:
+    pass
